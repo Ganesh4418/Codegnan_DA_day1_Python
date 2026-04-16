75f691d657adffcc2262e2ad8745b03a5d12934e
Ganesh4418 <mrleninganesh@gmail.com>
2026-04-16 10:43:52 +0530

    have discussed about variables, keywords, tokens

---
+++ Day1_python.py
@@ -0,0 +1,63 @@
+'''
+A=90
+print(A)
+_90=2
+print(_90)
+air_one=5
+print(air_one)
+re=8
+print(re)
+
+
+NOTES
+-----
+
+VARIABLES
+---------
+special characters,space,integers are not used as variables'''
+'''-->A using meaningful word for defining variables make easy identification
+
+KEYWORDS
+--------
+keywords are not used as variable name
+keyword are identifiers
+
+TOKENS
+------
+token is a small python program to complete task
+
+Comments
+--------
+1.single line comments
+--> used to explain a particular line of code, used along with a '#'
+
+2.multi-line comments
+---> used for puting multiple line more than one line in comments,
+handled by writing between ''' '''
+
+comments are not considerd by the cursor
+
+BOOLEAN
+-------
+this type of data type is used in the conditions where
+the code demands a 'yes or no' answer it return either TRUE or FALSE
+
+a,b,c=2,3,4
+print(a)
+print(b)
+print(c)
+
+TOKEN
+-----
+print(a+b) #this line prints addition operation
+print(b-c)
+print(a*b)
+print(a == c) #Boolean Type
+
+
+num=7
+if num%2 == 0:
+    print(f"{num} is EVEN")
+else:
+    print(f"{num} is ODD")
+'''
